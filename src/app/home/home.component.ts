793933079ec959d99936e9e0ef759534ac40b2c4
Harish Mangala <Harish.Mangala@davinta.com>
2017-09-01 19:24:20 +0530

    01092017

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -16,11 +16,14 @@ export class HomeComponent implements OnInit {
   addressFormCompleted: boolean;
   addressForm: FormGroup;
   contactForm: FormGroup;
-  formErrors = {
+  accountFormErrors = {
     username: '',
     email: '',
     passw: '',
-    confirmpassw: '',
+    confirmpassw: ''
+  };
+
+  addressFormErrors = {
     firstname: '',
     lastname: '',
     address1: '',
@@ -30,6 +33,7 @@ export class HomeComponent implements OnInit {
     pin: '',
     country: ''
   };
+
   validationMessages = {
     username: {
       required: 'Username is required.',
@@ -113,43 +117,63 @@ export class HomeComponent implements OnInit {
     });
     this.contactForm = this.fb.group({});
     // watch for changes and validate
-    this.accountForm.valueChanges.subscribe(data => this.validateForm(this.accountForm));
-    this.addressForm.valueChanges.subscribe(data => this.validateForm(this.addressForm));
+    this.accountForm.valueChanges.subscribe(data => this.validateForm(this.accountForm, this.accountFormErrors));
+    this.addressForm.valueChanges.subscribe(data => this.validateForm(this.addressForm, this.addressFormErrors));
   }
 
   /**
    * validate the entire form
    */
-  validateForm(form: FormGroup) {
-    console.log('form', form);
+  validateForm(form: FormGroup, formErrors) {
     let formValid = true;
-    for (const field in this.formErrors) {
+    let formCompleted = true;
+    for (const field in formErrors) {
       if (field) {
         // clear that input field errors
-        this.formErrors[field] = '';
+        formErrors[field] = '';
         // grab an input field by name
         const input = form.get(field);
-        if (input) {
-          if (input.invalid && input.dirty) {
-            // figure out the type of error
-            // loop over the formErrors field names
-            for (const error in input.errors) {
-              if (error) {
-                // assign that type of error message to a variable
-                if (formValid === true) {
-                  formValid = false;
+        if (input.invalid && input.dirty) {
+          // figure out the type of error
+          // loop over the formErrors field names
+          for (const error in input.errors) {
+            if (error) {
+              if (this.accountForm === form) {
+                if (this.accountFormCompleted === true) {
+                  console.log('account form error');
+                  this.accountFormCompleted = false;
                 }
-                this.formErrors[field] = this.validationMessages[field][error];
               }
+              if (this.addressForm === form) {
+                if (this.addressFormCompleted === true) {
+                  console.log('address form error');
+                  this.addressFormCompleted = false;
+                }
+              }
+              // assign that type of error message to a variable
+              if (formValid === true) {
+                formValid = false;
+              }
+              formErrors[field] = this.validationMessages[field][error];
             }
+          }
+        } else {
+          if (!input.dirty) {
+            formCompleted = false;
           }
         }
       }
     }
     if (this.accountForm === form) {
-      if (formValid === true) {
+      if (formValid === true && formCompleted === true) {
         console.log('account form completed');
         this.accountFormCompleted = true;
+      }
+    }
+    if (this.addressForm === form) {
+      if (formValid === true && formCompleted === true) {
+        console.log('address form completed');
+        this.addressFormCompleted = true;
       }
     }
   }
